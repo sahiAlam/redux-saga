e19501c575c14647fa13b30f4b124550c0a429f2
Md Sahi Alam Khan <ksahi4088@gmail.com>
2024-03-04 02:02:14 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getCatsFetch } from "./catState";
 
 function App() {
+  const cats = useSelector((state) => state.cats.cats);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getCatsFetch());
+  }, dispatch);
+
+  console.log(cats[0]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>CATS SPECIES GALLERY</h1>
+      <p>
+        Images of different species of cats. Lots of cats for your viewing
+        pleasure.
+      </p>
+      <hr />
+      <div className="Gallery">
+        {cats.map((cat) => (
+          <div key={cat.id} className="row">
+            <div className="column column-left">
+              <img
+                alt={cat.name}
+                // src={cat.image.url}
+                width={200}
+                height={200}
+              />
+            </div>
+            <div className="column column-right">
+              <h2>{cat.name}</h2>
+              <h5>Temperament: {cat.temperament}</h5>
+              <p>{cat.description}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
   );
 }
 
